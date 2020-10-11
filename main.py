8558ceba1c4a73af0f689eea0b484da29ae9e1a3
Adnan Arab <kisscussarab@gmail.com>
2020-10-11 19:31:52 +0100

    uvicorn addition

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/something")
+async def get_some_dictionary():
+    return {"Adnan": "Arab", "Something Else": "Something Else"}
+
 if __name__ == ("__main__"):
     uvicorn.run(app, host="127.0.0.1", port=9000)
